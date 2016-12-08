3c402c699a08bf86d9c22f48c16a7a0cf649c041
Jon Skidanov <jskidanov@westmont.edu>
2016-12-08 14:41:54 -0800

    Bad commit

---
+++ ally/feedback.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+		<meta name="mobile-web-app-capable" content="yes"/>
+		<meta name="apple-mobile-web-app-capable" content="yes"/>
+		<title>Ally - Feedback</title>
+		<meta name="description" content="An app to help people find food in Santa Barbara"/>
+		<meta name="author" content="Westmont Inspired Computing Lab"/>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+	</head>
+	<body>
+<<<<<<< HEAD:ally/feedback.html
+			<iframe class="col-md-12" src="https://docs.google.com/a/westmont.edu/forms/d/e/1FAIpQLSfS0P6rpnxvVuH0EyCnngUTKw29NHoE7rxGTBc67FHceIyYqQ/viewform?embedded=true" width="device-width" height="1100" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+			
+=======
+		<div class="container">
+			<div class="page-header">
+				<h3>Feedback</h3>
+			</div>
+		</div>
+		<div class="container text-center">
+			<div class="row-fluid">
+				<div class="col-md-12">
+		Hello From Ally! Santa Barbara's Helping Hand! Below, Please Enter your name, email, and your feedback and we will get back to you as soon as possible!<br><br>
+		<form action="URL to form script" method="POST">
+
+			Your name: <br>
+			<input type="text" name="realname"><br>
+			<br>
+
+			Your email: <br>
+			<input type="text" name="email"><br>
+			<br>
+
+			Your comments: <br>
+			<textarea name="comments" rows="15" cols="50"></textarea><br><br>
+
+			<input type="submit" value="Submit">
+			 
+			</form>
+				</div>
+			</div>
+		</div>
+
+		<footer class="footer">
+			<div class="container">
+				<div class="row-fluid text-center">
+					<div class="col-xs-12">
+						<a class="btn btn-default btn-lg" href="index.php" role="button" name="home_button">
+							<img class="bw_logo_button" src="img/\logos\black_and_white\black_on_clear.png"/>
+							Home
+						</a>
+					</div>
+		</footer>
+
+>>>>>>> d9e4e651eced65f298035fc9f3ebed6cf354bba7:ally/feedback.php
+		<!-- Load javascript at the end for speed -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
